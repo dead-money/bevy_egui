4ccd807619a40ced09a89477c232392f44d6b7fc
Brandon Reinhart <brandon@deadmoney.gg>
2024-10-20 12:27:13 -0700

    fix elapsed secs

--- src/systems.rs
+++ src/systems.rs
@@ -501,7 +501,7 @@ pub fn process_input_system(
 
     for mut context in context_params.contexts.iter_mut() {
         context.egui_input.modifiers = modifiers;
-        context.egui_input.time = Some(time.elapsed_seconds_f64());
+        context.egui_input.time = Some(time.elapsed_secs_f64());
     }
 
     // In some cases, we may skip certain events. For example, we ignore `ReceivedCharacter` events
